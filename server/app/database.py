0a5558c9bcf92dc63fc4df02f0e3b3be4daee1ae
Dhruv Rakeshbhai patel <dpatel5469@gmail.com>
2026-01-04 14:43:42 +0530

    Added `get_db` method to get database access.

--- server/app/database.py
+++ server/app/database.py
@@ -5,9 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/email_tracker")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://user:password@localhost:5432/email_tracker"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
